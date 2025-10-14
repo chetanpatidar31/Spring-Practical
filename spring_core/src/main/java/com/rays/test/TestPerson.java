579bebdba8e3b70a013502348107f47627420120
chetanpatidar31 <chetan50682@gmail.com>
2025-10-14 16:06:43 +0530

    IOC container bean

--- spring_core/src/main/java/com/rays/test/TestPerson.java
+++ spring_core/src/main/java/com/rays/test/TestPerson.java
@@ -1,7 +1,19 @@
 package com.rays.test;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
 public class TestPerson {
 
-	
+	public static void main(String[] args) {
+		
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("person.xml"));
+		
+		Person p = (Person) factory.getBean("person");
+		
+		System.out.println("Person's Name : "+p.getName());
+		System.out.println("Person's Address : "+p.getAddress());
+	}
 
 }
