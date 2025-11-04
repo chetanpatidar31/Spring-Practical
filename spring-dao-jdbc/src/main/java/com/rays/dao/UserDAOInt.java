d31cab7907c307a1fcf5037c38ec57110021b181
chetanpatidar31 <chetan50682@gmail.com>
2025-11-04 11:31:04 +0530

    DAO

---
+++ spring-dao-jdbc/src/main/java/com/rays/dao/UserDAOInt.java
@@ -0,0 +1,10 @@
+package com.rays.dao;
+
+import com.rays.dto.UserDTO;
+
+public interface UserDAOInt {
+
+	public long add(UserDTO userDTO);
+//	public void update(UserDTO userDTO);
+//	public void delete(long id);
+}
