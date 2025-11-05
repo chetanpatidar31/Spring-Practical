6b8d8162afe4f637a3e4ad6992b4c1e1efda0a58
chetanpatidar31 <chetan50682@gmail.com>
2025-11-05 17:40:40 +0530

    noType

---
+++ spring_core/src/main/java/com/rays/notype/UserDAOImpl.java
@@ -0,0 +1,10 @@
+package com.rays.notype;
+
+public class UserDAOImpl implements UserDAOInt{
+
+	public void add() {
+		System.out.println("add ...!!!!!");
+		
+	}
+
+}
---
+++ spring_core/src/main/java/com/rays/notype/UserDAOInt.java
@@ -0,0 +1,6 @@
+package com.rays.notype;
+
+public interface UserDAOInt {
+
+	public void add();
+}
