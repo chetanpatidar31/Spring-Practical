477019ed870f181e7d72feaea73cf2aa097c267a
chetanpatidar31 <chetan50682@gmail.com>
2025-11-11 17:40:22 +0530

    mvc-simple

---
+++ spring-mvc-simple/src/main/java/com/rays/ctl/WelcomeCtl.java
@@ -0,0 +1,18 @@
+package com.rays.ctl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "WelcomeCtl")
+public class WelcomeCtl {
+
+	@GetMapping
+	public String display(Model model) {
+		System.out.println("in display method");
+		model.addAttribute("message", "Welcome to Spring MVC!!!");
+		return "WelcomeView";
+	}
+}
