f0a6381b9de4e56924cb6b11ab66eea9d1377b01
chetanpatidar31 <chetan50682@gmail.com>
2025-11-14 01:33:25 +0530

    Register

---
+++ spring-mvc-simple/src/main/java/com/rays/ctl/UserRegistrationCtl.java
@@ -0,0 +1,43 @@
+package com.rays.ctl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.rays.dto.UserDTO;
+import com.rays.form.UserRegistrationForm;
+import com.rays.service.UserServiceInt;
+
+@Controller
+@RequestMapping(value = "Register")
+public class UserRegistrationCtl {
+
+	@Autowired
+	public UserServiceInt service;
+
+	@GetMapping
+	public String display(@ModelAttribute("form") UserRegistrationForm form) {
+		System.out.println("In Register display Method!!!!");
+		return "UserRegistration";
+	}
+
+	@PostMapping
+	public String submit(@ModelAttribute("form") UserRegistrationForm form, Model model) {
+
+		UserDTO dto = new UserDTO();
+		dto.setFirstName(form.getFirstName());
+		dto.setLastName(form.getLastName());
+		dto.setLogin(form.getLogin());
+		dto.setPassword(form.getPassword());
+		dto.setAddress(form.getAddress());
+
+		service.add(dto);
+		model.addAttribute("successMsg", "user register successfully");
+
+		return "UserRegistration";
+	}
+}
