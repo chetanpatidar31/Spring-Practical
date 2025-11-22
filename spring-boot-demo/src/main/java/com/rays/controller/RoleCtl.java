3e9420445e16f492cf0acae0a4839998bacb4b1d
chetanpatidar31 <chetan50682@gmail.com>
2025-11-22 08:34:01 +0530

    save

--- spring-boot-demo/src/main/java/com/rays/controller/RoleCtl.java
+++ spring-boot-demo/src/main/java/com/rays/controller/RoleCtl.java
@@ -3,12 +3,15 @@ package com.rays.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rays.common.BaseCtl;
 import com.rays.common.ORSResponse;
+import com.rays.dto.RoleDTO;
 import com.rays.form.RoleForm;
 import com.rays.service.RoleService;
 
@@ -18,6 +21,23 @@ public class RoleCtl extends BaseCtl {
 
 	@Autowired
 	public RoleService roleService;
-	
-	public ORSResponse save(@RequestBody @Valid RoleForm form)
+
+	@PostMapping("save")
+	public ORSResponse save(@RequestBody @Valid RoleForm form, BindingResult bindingResult) {
+
+		ORSResponse resp = validate(bindingResult);
+
+		if (!resp.isSuccess()) {
+			return resp;
+		}
+
+		RoleDTO dto = (RoleDTO) form.getDto();
+
+		long pk = roleService.add(dto);
+
+		resp.addData(pk);
+		resp.addMessage("Role added Successfully...!!!!");
+
+		return resp;
+	}
 }
