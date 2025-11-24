43a37b7f95376263904fdc8deff4b507058be42f
chetanpatidar31 <chetan50682@gmail.com>
2025-11-24 17:27:22 +0530

    user get

--- spring-boot-demo/src/main/java/com/rays/controller/UserCtl.java
+++ spring-boot-demo/src/main/java/com/rays/controller/UserCtl.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -49,11 +50,33 @@ public class UserCtl extends BaseCtl {
 
 		return resp;
 	}
-	
+
 	@PostMapping("delete/{ids}")
 	public ORSResponse delete(@PathVariable long[] ids) {
 		ORSResponse resp = new ORSResponse();
-		
+
+		for (long id : ids) {
+			userService.delete(id);
+		}
+
+		resp.addMessage("data deleted successfully");
+		resp.setSuccess(true);
+
+		return resp;
+	}
+
+	@GetMapping("get/{id}")
+	public ORSResponse get(@PathVariable long id) {
+
+		ORSResponse resp = new ORSResponse();
+
+		UserDTO dto = userService.findByPk(id);
+
+		if (dto != null) {
+			resp.addData(dto);
+			resp.setSuccess(true);
+		}
+
 		return resp;
 	}
 
